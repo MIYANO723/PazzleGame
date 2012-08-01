2e11bed4c5ae0ac0ec7fa6afb3cf5011618e4de6
MIYANO SYUNICHI <cryforyouing@yahoo.co.jp>
2012-08-01 18:01:31 +0900

    Stay stady
    Issues#2,#3,#10が終了  これから他のIssuesは動作しないと始まらないので、まずは動かす

--- puzzlegame.js
+++ puzzlegame.js
@@ -2,10 +2,11 @@
 window.onload = function(){
 	  var game = new Game(320,320);
 	  game.fps = 16;
-	  game.preload('puzzle.png','fire.png','Leftturn','Rightturn');
+	  game.preload('puzzle.png','fire.png','Leftturn.png','Rightturn.png');
 	  game.score = 0;
 	  var material;
 	  game.onload = function(){
+	    var map;
 		   game.addLabel = function(text, color, x, y){
 		     //ラベルの生成
 		     var label = new Label(text);
@@ -17,12 +18,8 @@ window.onload = function(){
 		     }
 		   //ゲームシステム文のグループ化
 		   var gamesystem = new Group();
-		   gamesystem.addChild();
-		   gamesystem.addChild();
-		   //操作コンソールグループ化
+		  //操作コンソールグループ化
 	     var gamecontrol = new Group();
-	     gamecontrol.addChild();
-	     gamecontrol.addChild();
 	     //背景の生成
 	     var bg = new Sprite(320, 320);
 	     bg.backgroundColor = "#87CEEB";
@@ -30,7 +27,7 @@ window.onload = function(){
      for (var i = 0;i<320; i += 16) {
         image.draw(game.assets["fire.png"], 7 * 16, 0, 16, 16, i, 320 - 16, 16, 16);
      };
-     
+      game.rootScene.addChild(bg);
    //パッドの生成
    var pad = new Pad();
    pad.x   = 220;
@@ -39,7 +36,7 @@ window.onload = function(){
    //マテリアルの生成
    var fall_flage = true;
 	 game.addBlock = function(x){
-		 var material = new Sprite(24, 24)
+		 var material = new Sprite(24, 24);
 		 material.image = game.assets['puzzle.png'];
 		 material.x = x;
 		 material.y = -16;
@@ -76,12 +73,11 @@ window.onload = function(){
 	        if ((game.tick % 10) === 0 ){
 	            var x = 155
 	            var speed = 6
-	            }
-	        label.text = "Time:" + Math.floor(game.tick / 16)  + "<BR>Score:" + game.score;
+	            label.text = "Time:" + Math.floor(game.tick / 16)  + "<BR>Score:" + game.score;
           } else if (game.tick === 0) {
             //ゲームオーバー画面の表示
             game.end(game.score, "あなたのスコアは" + game.score);
-        }
+        };
    };
   }
      //ゲーム開始
